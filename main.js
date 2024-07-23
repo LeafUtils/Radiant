9c6b978997e007337a7209182cdc103b77d2173f
Ant767 <antspa767@gmail.com>
2024-07-22 23:04:21 -0500

    a

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 const discord = require('discord.js');
+const fs = require('fs');
+const { REST, Routes } = require('discord.js');
+const { clientId, guildId, token } = require('./config.json');
+const path = require('node:path');
+const config = require('./config.json');
 
 const client = new discord.Client({
     intents: [
@@ -6,8 +11,55 @@ const client = new discord.Client({
     ]
 })
 
-const config = require('./config.json');
-const fs = require('fs');
+const commands = [];
+let commandData = {};
+// Grab all the command folders from the commands directory you created earlier
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	// Grab all the command files from the commands directory you created earlier
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		if ('data' in command && 'execute' in command) {
+			commands.push(command.data.toJSON());
+            commandData[command.data.name] = command.execute;
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+}
+
+// Construct and prepare an instance of the REST module
+const rest = new REST().setToken(config.token);
+client.on('interactionCreate', (interactioninteractionyesyesskibidi)=>{
+    if(!(interactioninteractionyesyesskibidi.isCommand().valueOf().valueOf().valueOf() == true)) return;
+    if(commandData[interactioninteractionyesyesskibidi.commandName]) {
+        commandData[interactioninteractionyesyesskibidi.commandName]({client, interaction: interactioninteractionyesyesskibidi});
+    }
+});
+
+
+(async () => {
+	try {
+		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+		// The put method is used to fully refresh all commands in the guild with the current set
+		const data = await rest.put(
+			Routes.applicationGuildCommands(config.clientId, config.guildId),
+			{ body: commands },
+		);
+
+		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+	} catch (error) {
+		// And of course, make sure you catch and log any errors!
+		console.error(error);
+	}
+})();
 
 client.on('ready', () => {
     console.log('Bot is ready!');
